b858f8be412f6eb4539041cd3e6f4f4350589dec
Izabella Novikova <belka215@gmail.com>
2025-05-11 16:54:29 +0200

    init:hw

--- src/components/pokemon-card/index.tsx
+++ src/components/pokemon-card/index.tsx
@@ -3,15 +3,21 @@ import { Link } from "react-router-dom";
 import { IPokemonComponent } from "../../typings/pokemon"
 import like from "./img/like.png"
 import compare from "./img/compare.png"
+import { useDispatch } from "react-redux";
+import { setPokemonURL } from "../../store/pokemons/slice";
 import "./index.scss"
 
 export const PokemonCard: FC<IPokemonComponent> = ({ pokemon }) => {
+  const dispatch = useDispatch();
+  const array = pokemon.url.split('/');
+  const id = array[array.length - 2];
+
   const handleClick = () => {
-    console.log(pokemon.name)
+    dispatch(setPokemonURL(pokemon.url));
   }
 
   return (
-    <Link to={`/pokemons`} className="card" onClick={handleClick}>
+    <Link to={`/pokemons/${id}`} className="card" onClick={handleClick}>
       <div className="card__top">
         <h4 className="card__name">{pokemon.name}</h4>
       </div>
--- src/typings/pokemon.ts
+++ src/typings/pokemon.ts
@@ -6,22 +6,3 @@ export type PokemonType = {
 export interface IPokemonComponent {
   pokemon: PokemonType;
 }
-
-export type DetailedPokemonType = {
-  name: string;
-  weight: number;
-  height: number;
-  isFavorite: boolean;
-  isInComparison: boolean;
-  img?: string;
-  stats?: Stat[];
-}
-
-export interface IDetailedPokemonComponent {
-  pokemon: DetailedPokemonType;
-}
-
-type Stat = {
-  name: string;
-  value: number;
-}
